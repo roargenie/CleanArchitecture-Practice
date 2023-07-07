1101691a74c795ef4d5169b0d8842efba8c18f5b
roargenie <lazyhoper987@gmail.com>
2023-07-07 19:40:01 +0900

    git init

---
+++ CleanArchitecturePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanArchitecturePractice
+//
+//  Created by 이명진 on 2023/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
